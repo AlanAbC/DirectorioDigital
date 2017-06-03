a2b48b5bc7905c4cfabb79672de6f31d7a7e358a
aabundis <alanjiraya77@gmail.com>
2017-06-02 22:01:08 -0500

    Modificacion Mitad de Interfaz

--- app/src/main/java/com/claresti/directoriodigital/MapaPrincipal.java
+++ app/src/main/java/com/claresti/directoriodigital/MapaPrincipal.java
@@ -1,7 +1,14 @@
 package com.claresti.directoriodigital;
 
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +17,19 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MapaPrincipal extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    //Menu, Declaracion de variables
+    private DrawerLayout drawerLayout;
+    final List<MenuItem> items = new ArrayList<>();
+    private Menu menu;
+    private Menu menuCat;
+    private ImageView btnMenu;
+    private NavigationView nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +39,13 @@ public class MapaPrincipal extends FragmentActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.dLayout);
+
+        //Asignacion de vistas a las variables de Menu
+        nav = (NavigationView) findViewById(R.id.navigation);
+        menu = nav.getMenu();
+        menuCat= menu.getItem(0).getSubMenu();
     }
 
 
@@ -42,5 +66,10 @@ public class MapaPrincipal extends FragmentActivity implements OnMapReadyCallbac
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    //Funcion para agregar items al menu solo resta establecer como se manejaran los iconos
+    public void agregarMenuItem(String nombreItem){
+        menuCat.add(nombreItem).setIcon(R.drawable.menu);
     }
 }
